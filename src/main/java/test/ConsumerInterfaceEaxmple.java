660464626ea09c4c92876a541db134ceef614c04
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-18 15:37:41 +0330

    Write method use Stream

--- src/main/java/test/ConsumerInterfaceEaxmple.java
+++ src/main/java/test/ConsumerInterfaceEaxmple.java
@@ -4,6 +4,6 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class ConsumerInterfaceEaxmple {
     public static void addlist(List<Integer> list){
-
+        System.out.println(list.stream().mapToInt(Integer::intValue).sum());
     }
 }
